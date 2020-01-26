3e50bf639410ee77bb245fdf7625dff0e8421b93
FrogAc <acgfrog@outlook.com>
2020-01-26 03:03:10 -0800

    add pcx

---
+++ unity/Assets/WorkingAdditions/Scripts/GeneratePoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePoints : MonoBehaviour
+{
+    public Texture2D[] textures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
